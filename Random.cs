d5470557b08e04e528852cf0321048ab2bcab7f0
Élie BRAMI <elie.brami@epita.fr>
2019-04-17 03:41:47 +0200

    begin semi-random generator

--- Random.cs
+++ Random.cs
@@ -6,20 +6,19 @@ namespace FlappyRunner
     {
         public override int Next()
         {
-            Console.WriteLine("variant 0");
+            //Console.WriteLine("variant 0");
             return base.Next();
         }
 
         public override int Next(int maxValue)
         {
-            Console.WriteLine("variant 1");
+            //Console.WriteLine("variant 1");
             return base.Next(maxValue);
         }
 
         public override int Next(int minValue, int maxValue)
         {
-            Console.WriteLine("variant 2");
-            return base.Next(minValue, maxValue);
+            return Next(maxValue - minValue) + minValue;
         }
     }
 }
